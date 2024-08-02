334e43ab697e6de96e697307b834d4211f164372
Seth <seth.aaron.olson@gmail.com>
2024-08-02 16:01:25 -0700

    add random beginnings array and selection

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const beg = [
+    'It is said',
+    'To live a better life',
+    'Happiness starts with'
+]
+
+const randBeg = beg[Math.floor(Math.random() * beg.length)];
+console.log(randBeg);
